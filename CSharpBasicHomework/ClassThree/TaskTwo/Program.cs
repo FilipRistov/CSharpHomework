1e0f995dc95c8f92fc0177be791c13a6547ae240
FilipRistov <ristovfilip@gmail.com>
2022-03-22 16:57:11 +0100

    Inital

---
+++ CSharpBasicHomework/ClassThree/TaskTwo/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace TaskTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] studentsGOne = { "Zdravko", "Petko", "Stanko", "Branko", "Trajko" };
+            string[] studentsGTwo = { "Zoki", "Mile", "Brus", "Petko", "Vele" };
+            Console.WriteLine("Enter student group:(1 or 2)");
+            string groupInput = Console.ReadLine();
+            if (groupInput == "1")
+            {
+                Console.WriteLine("The Students in G1 are:");
+                foreach (string element in studentsGOne)
+                {
+                    Console.WriteLine(element);
+                }
+            }
+            else if (groupInput == "2")
+            {
+                Console.WriteLine("The Students in G2 are:");
+                foreach (string element in studentsGTwo)
+                {
+                    Console.WriteLine(element);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid student group number");
+            }
+        }
+    }
+}
---
+++ CSharpBasicHomework/ClassThree/Task_One/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ClassThree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[6];
+            int sum = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine($"Enter integer no.{i + 1}:");
+                string number = Console.ReadLine();
+                int.TryParse(number, out int parsed);
+                array[i] = parsed;
+                if (array[i] % 2 == 0)
+                {
+                    sum += array[i];
+                }
+            }
+            Console.WriteLine($"The result is: {sum}");
+        }
+    }
+}
